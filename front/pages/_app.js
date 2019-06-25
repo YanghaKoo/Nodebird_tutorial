3b9f0b9a7b73dcc30fa8bcc0422ebbbfb8158017
koo <yangha93@naver.com>
2019-06-26 03:47:54 +0900

    chapter 3 redux

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -6,9 +6,15 @@ import Head from "next/head";
 import AppLayout from "../components/AppLayout";
 import PropTypes from 'prop-types'
 
-const NodeBird = ({ Component }) => {
+import reducer from '../reducers'
+import { Provider } from 'react-redux'
+
+import withRedux from 'next-redux-wrapper'
+import {createStore} from 'redux'
+
+const NodeBird = ({ Component, store }) => {
   return (
-    <>
+    <Provider store={store}>
       {/*  antd css 파일 헤드에 넣어주기!! */}
       <Head>
         <title>Nodebird</title>
@@ -21,7 +27,7 @@ const NodeBird = ({ Component }) => {
       <AppLayout>
         <Component />   {/* <-- 이 부분이 자식 컴포넌트들 들어갈 자리!!! 이 사이에 공통되는 부분들이 들어감*/}
       </AppLayout>
-    </>
+    </Provider>
   );
 };
 
@@ -29,4 +35,9 @@ NodeBird.propTypes = {
   Component : PropTypes.elementType // jsx에 들어갈 수 있는 모든 것을 elementType이라고 함
 }
 
-export default NodeBird;
+// 이 부분은 그냥 외우기
+export default withRedux((initialState, options)=>{
+  const store = createStore(reducer, initialState)
+  // 여기에 store 커스터마이징 가능, 기본적으로는 이렇게
+  return store;
+})(NodeBird)
